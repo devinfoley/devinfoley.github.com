5254e23d3b4073d571e5b17d9a50274a1f72e717
Devin Foley <devin@devinfoley.com>
2012-02-22 14:47:21 -0800

    Clean things up a bit and make print easier.

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.job .head').click(function() {
+    var speed = 'fast';
+    var head = $(this);
+    var body = head.parent().find('.body');
+    if (body.is(':visible')) {
+      body.slideUp(speed);
+      head.find('.drop').addClass('up');
+    } else {
+      body.slideDown(speed);
+      head.find('.drop').removeClass('up');
+    }
+  });
+  $('.job .body').hide();
+});
